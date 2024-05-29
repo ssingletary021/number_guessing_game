aec1d7b10dd8370bf716054963df4d0e4abdcc03
Shamel Jones <ssingletary021@gmail.com>
2024-05-29 17:31:22 +0000

    feat: Implement user interaction and game logic for number guessing game

--- number_guess.sh
+++ number_guess.sh
@@ -1,2 +1,33 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+# prompt for username and read
+
+# check if new username
+
+# if new, add to database
+
+# if not new
+
+
+#random number generation
+
+# prompt for guess
+
+# if guess is higher than secret number
+
+# prompt for guess
+
+# if guess is lower than secret number
+
+# prompt for new guess
+
+# if input is not an integer
+
+# when guess is right
+
+# add to database as games_played
+
+# must tally guesses to be added to database as number_of_guesses
+
+# best_game = gameplay with least guesses
